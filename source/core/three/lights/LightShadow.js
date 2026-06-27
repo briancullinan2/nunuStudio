15ca0ece3adb213552af64de555cac0993fc9ba1
Brian Cullinan <megamindbrian@gmail.com>
2026-06-27 11:48:26 -0700

    .gui.updateInterface() not defined means i'm in my async code, need to follow the chain and await

--- source/core/three/lights/LightShadow.js
+++ source/core/three/lights/LightShadow.js
@@ -1,52 +1,60 @@
-THREE.LightShadow.prototype.toJSON = function()
+import { LightShadow } from "three/src/lights/LightShadow.js";
+
+export class CustomLightShadow extends LightShadow
 {
-	var data = {};
-
-	data.bias = this.bias;
-	data.radius = this.radius;
-
-	data.mapSize =
+	/**
+	 * Serialize the shadow properties to a clean object.
+	 * @method toJSON
+	 * @return {Object}
+	 */
+	toJSON()
 	{
-		x: this.mapSize.x,
-		y: this.mapSize.y
-	};
-	
-	data.camera =
-	{
-		top: this.camera.top,
-		bottom: this.camera.bottom,
-		far: this.camera.far,
-		near: this.camera.near,
-		left: this.camera.left,
-		right: this.camera.right
-	};
-
-	return data;
-};
-
-THREE.LightShadow.prototype.fromJSON = function(data)
-{
-	if (data.bias !== undefined)
-	{
-		this.bias = data.bias;
+		return {
+			bias: this.bias,
+			radius: this.radius,
+			mapSize: {
+				x: this.mapSize.x,
+				y: this.mapSize.y
+			},
+			camera: {
+				top: this.camera.top,
+				bottom: this.camera.bottom,
+				far: this.camera.far,
+				near: this.camera.near,
+				left: this.camera.left,
+				right: this.camera.right
+			}
+		};
 	}
-	if (data.radius !== undefined)
-	{	
-		this.radius = data.radius;
-	}
-	if (data.mapSize !== undefined)
+
+	/**
+	 * Hydrate the shadow properties from a data object.
+	 * @method fromJSON
+	 * @param {Object} data
+	 */
+	fromJSON(data)
 	{
-		var mapSize = data.mapSize;
-		this.mapSize.set(mapSize.x, mapSize.y);
+		if(data.bias !== undefined)
+		{
+			this.bias = data.bias;
+		}
+		if(data.radius !== undefined)
+		{
+			this.radius = data.radius;
+		}
+		if(data.mapSize !== undefined)
+		{
+			this.mapSize.set(data.mapSize.x, data.mapSize.y);
+		}
+		if(data.camera !== undefined)
+		{
+			const cam = data.camera;
+			this.camera.top = cam.top;
+			this.camera.bottom = cam.bottom;
+			this.camera.left = cam.left;
+			this.camera.right = cam.right;
+			this.camera.near = cam.near;
+			this.camera.far = cam.far;
+		}
 	}
-	if (data.camera !== undefined)
-	{
-		var camera = data.camera;
-		this.camera.top = camera.top;
-		this.camera.bottom = camera.bottom;
-		this.camera.left = camera.left;
-		this.camera.right = camera.right;
-		this.camera.near = camera.near;
-		this.camera.far = camera.far;
-	}
-};
+}
