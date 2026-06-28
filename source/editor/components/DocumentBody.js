72cc4280b3ff3913da014f79c47c8b9514721ce6
Brian Cullinan <megamindbrian@gmail.com>
2026-06-28 13:11:20 -0700

    fix for layout thrashing, GUI update throttle

--- source/editor/components/DocumentBody.js
+++ source/editor/components/DocumentBody.js
@@ -1,15 +1,16 @@
-import {Vector2} from "three";
-import {Component} from "./Component.js";
+import { Vector2 } from "three";
+import { Component } from "./Component.js";
+
 
 /**
- * Body to represent the document.body 
- * 
+ * Body to represent the document.body
+ *
  * It does not extend the Component class, but has the same base attributes and can be used as a parent for components.
  *
  * @static
  * @class DocumentBody
  */
-var DocumentBody = 
+var DocumentBody =
 {
 	parent: null,
 	element: document.body,
@@ -17,20 +18,30 @@ var DocumentBody =
 	isComponent: true,
 	visible: true,
 	position: new Vector2(0, 0),
-	_size: new Vector2(0, 0)
+	_size: new Vector2(0, 0),
+	// Top-level layout cache memory object to eliminate browser style/layout thrashing reflows
+	innerWidth: window.innerWidth,
+	innerHeight: window.innerHeight
 };
 
 Object.defineProperties(DocumentBody,
 	{
 		size:
-	{
-		get: function()
 		{
-			this._size.set(window.innerWidth, window.innerHeight);
-			return this._size;
-		},
-		set: function() {}
-	}
+			get: function () {
+				// Reads from the lightweight cache instead of forcing a full browser style recalc loop
+				this._size.set(canvasSize.innerWidth, canvasSize.innerHeight);
+				return this._size;
+			},
+			set: function () { }
+		}
 	});
 
-export {DocumentBody};
+// Top-level independent layout observer running natively in both web and desktop environments
+var layoutObserver = new ResizeObserver(function (entries) {
+	DocumentBody.innerWidth = document.body.clientWidth || window.innerWidth;
+	DocumentBody.innerHeight = document.body.clientHeight || window.innerHeight;
+});
+layoutObserver.observe(document.body);
+
+export { DocumentBody };
--- source/editor/history/action/resources/AddResourceAction.js
+++ source/editor/history/action/resources/AddResourceAction.js
@@ -12,10 +12,12 @@ import { Editor } from "../../../Editor.js";
  * @param {ResourceManager} manager Manager to insert the resource into.
  * @param {string} category Category of the resource.
  */
-class AddResourceAction extends Action
-{
-	constructor(resource, manager, category)
-	{
+class AddResourceAction extends Action {
+	// Native ES6 static property declarations
+	static _lastGuiCallTime = 0;
+	static _guiThrottleTimeout = null;
+
+	constructor(resource, manager, category) {
 		super();
 
 		this.resource = resource;
@@ -23,31 +25,63 @@ class AddResourceAction extends Action
 		this.category = category;
 	}
 
-	apply()
-	{
+	apply() {
 		ResourceCrawler.addResource(this.manager, this.resource, this.category);
 
-		AddResourceAction.updateGUI();
+		AddResourceAction.throttledUpdateGUI();
 	}
 
-	async revert()
-	{
+	async revert() {
 		const { RemoveResourceAction } = await import("./RemoveResourceAction.js");
 
 		ResourceCrawler.removeResource(this.manager, this.resource, this.category);
 
-		if(this.resource.dispose !== undefined)
-		{
+		if(this.resource.dispose !== undefined) {
 			this.resource.dispose();
 		}
 
-		RemoveResourceAction.updateGUI();
+		AddResourceAction.throttledUpdateGUI();
 	}
 
+	/**
+	 * Standard un-throttled immediate view update.
+	 *
+	 * @static
+	 * @method updateGUI
+	 */
+	static updateGUI() {
+		Editor.updateObjectsViewsGUI();
+	}
+
+	/**
+	 * High-frequency performance protector that throttles interface calculations
+	 * to a maximum boundary of once per second (1000ms).
+	 *
+	 * @static
+	 * @method throttledUpdateGUI
+	 */
+	static throttledUpdateGUI() {
+		const now = performance.now();
+		const remaining = 1000 - (now - AddResourceAction._lastGuiCallTime);
+
+		// Scenario A: First call or enough time has passed -> Update immediately!
+		if(remaining <= 0 && !window.isLoadingBSP) {
+			if(AddResourceAction._guiThrottleTimeout) {
+				clearTimeout(AddResourceAction._guiThrottleTimeout);
+				AddResourceAction._guiThrottleTimeout = null;
+			}
+			AddResourceAction._lastGuiCallTime = now;
+			AddResourceAction.updateGUI();
+		}
+		// Scenario B: Called too quickly -> Queue up a guaranteed trailing edge update
+		else if(!AddResourceAction._guiThrottleTimeout && !window.isLoadingBSP) {
+			AddResourceAction._guiThrottleTimeout = setTimeout(function () {
+				AddResourceAction._lastGuiCallTime = performance.now();
+				AddResourceAction._guiThrottleTimeout = null;
+				AddResourceAction.updateGUI();
+			}, remaining);
+		}
+	}
 }
 
-AddResourceAction.updateGUI = function ()
-{
-	Editor.updateObjectsViewsGUI();
-};
 export { AddResourceAction };
